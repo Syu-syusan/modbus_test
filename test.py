3560c23e13b09e25bd881c68ec08d981a6f51b1c
Syu-syusan <syusyusan@outlook.jp>
2024-02-20 15:13:57 +0900

    testcode edit

--- test.py
+++ test.py
@@ -4,19 +4,16 @@ from pymodbus.client.tcp import ModbusTcpClient
 HOST = '192.168.85.201'
 PORT = 502
 
-# Modbus TCPクライアントのインスタンスを作成
+# Modbusインスタンスを作成
 client = ModbusTcpClient(HOST, port=PORT)
 
 # 接続を開始
 client.connect()
 
-# 単一のレジスタを読み取り（例: インバータ状態／制御入力命令）
-# 40008はModbusアドレスですが、アドレスのオフセットに注意してください。
-# pymodbusではアドレスが0から始まるため、1を引いた値を使用する必要があります。
+# 単一のレジスタを読み取り
 address = 0
-ADDRESS_START = 0  # Modbusアドレスのオフセット調整
+ADDRESS_START = 0
 ADDRESS_END = 100
-  # unitはModbusデバイスIDです。
 
 # 結果を表示
 for address in range(ADDRESS_START, ADDRESS_END):
